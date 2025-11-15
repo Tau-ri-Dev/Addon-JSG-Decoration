1a66ae5f663f996a8d041fddc89f2d717e99b98c
MineDragonCZ_ <minedragoncz@email.cz>
2025-11-15 18:43:20 +0100

    Removed tooltip from braziers

--- src/main/java/dev/tauri/jsgdecor/common/item/BrazierItem.java
+++ src/main/java/dev/tauri/jsgdecor/common/item/BrazierItem.java
@@ -1,23 +1,16 @@
 package dev.tauri.jsgdecor.common.item;
 
-import dev.tauri.jsg.helpers.ItemHelper;
 import dev.tauri.jsg.item.JSGBlockItem;
 import dev.tauri.jsg.item.JSGModelOBJInGUIRenderer;
 import dev.tauri.jsgdecor.client.renderer.brazier.BrazierItemRenderer;
 import dev.tauri.jsgdecor.common.block.BrazierType;
 import dev.tauri.jsgdecor.common.registry.TabRegistry;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 
-import javax.annotation.Nullable;
-import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -44,11 +37,5 @@ public class BrazierItem extends JSGBlockItem {
                 return instance;
             }
         });
-    }
-
-    @Override
-    @ParametersAreNonnullByDefault
-    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag tooltipFlag) {
-        ItemHelper.applyGenericToolTip(this.getDescriptionId(), components, tooltipFlag);
     }
 }
