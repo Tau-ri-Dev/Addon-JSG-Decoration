dfa8897861a3700e03f1a69eb155d478696c5473
Fredyman95 <93224497+Fredyman95@users.noreply.github.com>
2026-07-17 22:27:49 +0200

    Started work on ItemTagsGenerator - MOD CAN'T BE COMPILED!

--- src/main/java/dev/tauri/jsgdecor/datagen/tag/JSGDecorItemTagGenerator.java
+++ src/main/java/dev/tauri/jsgdecor/datagen/tag/JSGDecorItemTagGenerator.java
@@ -1,10 +1,17 @@
 package dev.tauri.jsgdecor.datagen.tag;
 
 import dev.tauri.jsgdecor.JSGDecor;
+import dev.tauri.jsgdecor.common.block.BrazierType;
+import dev.tauri.jsgdecor.common.block.WoodBlock;
+import dev.tauri.jsgdecor.common.boat.BoatTypes;
+import dev.tauri.jsgdecor.common.registry.JSGDecorBlocks;
+import dev.tauri.jsgdecor.common.registry.tag.JSGDecorItemTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
 
@@ -19,82 +26,95 @@ public class JSGDecorItemTagGenerator extends ItemTagsProvider {
     @Override
     @ParametersAreNonnullByDefault
     protected void addTags(HolderLookup.Provider pProvider) {
+
+        //Boats
+        for (BoatTypes type : BoatTypes.values()) {
+            tag(JSGDecorItemTags.BOATS).add(type.getDrop(false));
+            tag(JSGDecorItemTags.CHEST_BOATS).add(type.getDrop(true));
+        }
+
+        //WOOD
+        for (WoodBlock.Material material : WoodBlock.Material.values()) {
+            tag(JSGDecorItemTags.LOGS)
+                    .add(JSGDecorBlocks.WOOD_BLOCKS.get(material.getMaterial() + "_" + "log").get().asItem())
+                    .add(JSGDecorBlocks.WOOD_BLOCKS.get("stripped_" + material.getMaterial() + "_" + "log").get().asItem())
+                    .add(JSGDecorBlocks.WOOD_BLOCKS.get(material.getMaterial() + "_" + "wood").get().asItem())
+                    .add(JSGDecorBlocks.WOOD_BLOCKS.get("stripped_" + material.getMaterial() + "_" + "wood").get().asItem());
+
+            if (material.isFlammable()) {
+                tag(JSGDecorItemTags.LOGS_THAT_BURN)
+                        .add(JSGDecorBlocks.WOOD_BLOCKS.get(material.getMaterial() + "_" + "log").get().asItem())
+                        .add(JSGDecorBlocks.WOOD_BLOCKS.get("stripped_" + material.getMaterial() + "_" + "log").get().asItem())
+                        .add(JSGDecorBlocks.WOOD_BLOCKS.get(material.getMaterial() + "_" + "wood").get().asItem())
+                        .add(JSGDecorBlocks.WOOD_BLOCKS.get("stripped_" + material.getMaterial() + "_" + "wood").get().asItem());
+            }
+
+            tag(JSGDecorItemTags.LEAVES).add(JSGDecorBlocks.WOOD_BLOCKS.get(material.getMaterial() + "_" + "leaves").get().asItem());
+            tag(JSGDecorItemTags.SAPLINGS).add(JSGDecorBlocks.WOOD_BLOCKS.get(material.getMaterial() + "_" + "sapling").get().asItem());
+            tag(JSGDecorItemTags.PLANKS).add(JSGDecorBlocks.WOOD_BLOCKS.get(material.getMaterial() + "_" + "planks").get().asItem());
+            tag(JSGDecorItemTags.WOODEN_SLABS).add(JSGDecorBlocks.WOOD_BLOCKS.get(material.getMaterial() + "_" + "slab").get().asItem());
+            tag(JSGDecorItemTags.WOODEN_STAIRS).add(JSGDecorBlocks.WOOD_BLOCKS.get(material.getMaterial() + "_" + "stairs").get().asItem());
+            tag(JSGDecorItemTags.WOODEN_FENCES).add(JSGDecorBlocks.WOOD_BLOCKS.get(material.getMaterial() + "_" + "fence").get().asItem());
+            tag(JSGDecorItemTags.WOODEN_FENCE_GATES).add(JSGDecorBlocks.WOOD_BLOCKS.get(material.getMaterial() + "_" + "fence_gate").get().asItem());
+            tag(JSGDecorItemTags.WOODEN_BUTTONS).add(JSGDecorBlocks.WOOD_BLOCKS.get(material.getMaterial() + "_" + "button").get().asItem());
+            tag(JSGDecorItemTags.WOODEN_PRESSURE_PLATES).add(JSGDecorBlocks.WOOD_BLOCKS.get(material.getMaterial() + "_" + "pressure_plate").get().asItem());
+            tag(JSGDecorItemTags.WOODEN_DOORS).add(JSGDecorBlocks.WOOD_BLOCKS.get(material.getMaterial() + "_" + "door").get().asItem());
+            tag(JSGDecorItemTags.WOODEN_TRAPDOORS).add(JSGDecorBlocks.WOOD_BLOCKS.get(material.getMaterial() + "_" + "trapdoor").get().asItem());
+        }
+
+
+
+
+        var buttonsTag = tag(JSGDecorItemTags.BUTTONS).addTag(JSGDecorItemTags.WOODEN_BUTTONS);
+        var doorsTag = tag(JSGDecorItemTags.DOORS).addTag(JSGDecorItemTags.WOODEN_DOORS);
+        var trapdoorsTag = tag(JSGDecorItemTags.TRAPDOORS).addTag(JSGDecorItemTags.WOODEN_TRAPDOORS);
+        var fencesTag = tag(JSGDecorItemTags.FENCES).addTag(JSGDecorItemTags.WOODEN_FENCES);
+        var fenceGatesTag = tag(JSGDecorItemTags.FENCE_GATES).addTag(JSGDecorItemTags.WOODEN_FENCE_GATES);
+        var slabsTag = tag(JSGDecorItemTags.SLABS).addTag(JSGDecorItemTags.WOODEN_SLABS);
+        var stairsTag = tag(JSGDecorItemTags.STAIRS).addTag(JSGDecorItemTags.WOODEN_STAIRS);
+
+
+    //minecraft tags
+
+        //Braziers
+        tag(ItemTags.PIGLIN_LOVED).add(BrazierType.HATAK.block().get().asItem());
+
+        //Boats
+        tag(ItemTags.BOATS).addTag(JSGDecorItemTags.BOATS);
+        tag(ItemTags.CHEST_BOATS).addTag(JSGDecorItemTags.CHEST_BOATS);
+
+        //Wood
+        tag(ItemTags.LOGS).addTag(JSGDecorItemTags.LOGS);
+        tag(ItemTags.LOGS_THAT_BURN).addTag(JSGDecorItemTags.LOGS_THAT_BURN);
+        tag(ItemTags.LEAVES).addTag(JSGDecorItemTags.LEAVES);
+        tag(ItemTags.SAPLINGS).addTag(JSGDecorItemTags.SAPLINGS);
+        tag(ItemTags.PLANKS).addTag(JSGDecorItemTags.PLANKS);
+        tag(ItemTags.WOODEN_SLABS).addTag(JSGDecorItemTags.WOODEN_SLABS);
+        tag(ItemTags.WOODEN_STAIRS).addTag(JSGDecorItemTags.WOODEN_STAIRS);
+        tag(ItemTags.WOODEN_FENCES).addTag(JSGDecorItemTags.WOODEN_FENCES);
+        tag(ItemTags.WOODEN_BUTTONS).addTag(JSGDecorItemTags.WOODEN_BUTTONS);
+        tag(ItemTags.WOODEN_PRESSURE_PLATES).addTag(JSGDecorItemTags.WOODEN_PRESSURE_PLATES);
+        tag(ItemTags.WOODEN_DOORS).addTag(JSGDecorItemTags.WOODEN_DOORS);
+        tag(ItemTags.WOODEN_TRAPDOORS).addTag(JSGDecorItemTags.WOODEN_TRAPDOORS);
+
+
+
+    //forge tags
+
+        tag(Tags.Items.FENCE_GATES_WOODEN).addTag(JSGDecorItemTags.WOODEN_FENCE_GATES); //mozna i do forge tagu fence gate
+
+
+
+
+
+
+
+
+
+
         //JSGDecor wood and wood products tags
-        /*tag(JSGDecorItemTags.BOATS)
-                .add(BoatTypes.LEMON.getDrop(false))
-                .addTag(JSGDecorItemTags.CHEST_BOATS);
 
-        tag(JSGDecorItemTags.BUTTONS)
-                .addTag(JSGDecorItemTags.WOODEN_BUTTONS);
 
-        tag(JSGDecorItemTags.CHEST_BOATS)
-                .add(BoatTypes.LEMON.getDrop(true));
-
-        tag(JSGDecorItemTags.DOORS)
-                .addTag(JSGDecorItemTags.WOODEN_DOORS);
-
-        tag(JSGDecorItemTags.FENCE_GATES)
-                .addTag(JSGDecorItemTags.WOODEN_FENCE_GATES);
-
-        tag(JSGDecorItemTags.FENCES)
-                .addTag(JSGDecorItemTags.WOODEN_FENCES);
-
-        tag(JSGDecorItemTags.LEAVES)
-                .addTag(JSGDecorItemTags.LEMON_LEAVES);
-
-        tag(JSGDecorItemTags.LEMON_LEAVES)
-                .add(JSGDecorBlocks.LEMON_LEAVES.get().asItem());
-
-        tag(JSGDecorItemTags.LOGS)
-                .addTag(JSGDecorItemTags.LEMON_LOGS);
-
-        tag(JSGDecorItemTags.LEMON_LOGS)
-                .add(JSGDecorBlocks.LEMON_LOG.get().asItem())
-                .add(JSGDecorBlocks.LEMON_WOOD.get().asItem())
-                .add(JSGDecorBlocks.LEMON_LOG_STRIPPED.get().asItem())
-                .add(JSGDecorBlocks.LEMON_WOOD_STRIPPED.get().asItem());
-
-        tag(JSGDecorItemTags.LOGS_THAT_BURN)
-                .addTag(JSGDecorItemTags.LEMON_LOGS);
-
-        tag(JSGDecorItemTags.PLANKS)
-                .add(JSGDecorBlocks.LEMON_PLANKS.get().asItem());
-
-        tag(JSGDecorItemTags.SAPLINGS)
-                .add(JSGDecorBlocks.LEMON_SAPLING.get().asItem());
-
-        tag(JSGDecorItemTags.SLABS)
-                .addTag(JSGDecorItemTags.WOODEN_SLABS);
-
-        tag(JSGDecorItemTags.STAIRS)
-                .addTag(JSGDecorItemTags.WOODEN_STAIRS);
-
-        tag(JSGDecorItemTags.TRAPDOORS)
-                .addTag(JSGDecorItemTags.WOODEN_TRAPDOORS);
-
-        tag(JSGDecorItemTags.WOODEN_BUTTONS)
-                .add(JSGDecorBlocks.LEMON_BUTTON.get().asItem());
-
-        tag(JSGDecorItemTags.WOODEN_DOORS)
-                .add(JSGDecorBlocks.LEMON_DOOR.get().asItem());
-
-        tag(JSGDecorItemTags.WOODEN_FENCES)
-                .add(JSGDecorBlocks.LEMON_FENCE.get().asItem());
-
-        tag(JSGDecorItemTags.WOODEN_FENCE_GATES)
-                .add(JSGDecorBlocks.LEMON_GATE.get().asItem());
-
-        tag(JSGDecorItemTags.WOODEN_PRESSURE_PLATES)
-                .add(JSGDecorBlocks.LEMON_PRESSURE_PLATE.get().asItem());
-
-        tag(JSGDecorItemTags.WOODEN_SLABS)
-                .add(JSGDecorBlocks.LEMON_SLAB.get().asItem());
-
-        tag(JSGDecorItemTags.WOODEN_STAIRS)
-                .add(JSGDecorBlocks.LEMON_STAIRS.get().asItem());
-
-        tag(JSGDecorItemTags.WOODEN_TRAPDOORS)
-                .add(JSGDecorBlocks.LEMON_TRAPDOOR.get().asItem());
 
         //JSGDecor glass tags
         tag(JSGDecorItemTags.GLASS)
@@ -133,14 +153,10 @@ public class JSGDecorItemTagGenerator extends ItemTagsProvider {
         }
 
         //minecraft wood and wood products tags
-        tag(ItemTags.BOATS)
-                .addTag(JSGDecorItemTags.BOATS);
+
 
         tag(ItemTags.BUTTONS)
                 .addTag(JSGDecorItemTags.BUTTONS);
-
-        tag(ItemTags.CHEST_BOATS)
-                .addTag(JSGDecorItemTags.CHEST_BOATS);
 
         tag(ItemTags.COMPLETES_FIND_TREE_TUTORIAL)
                 .addTag(JSGDecorItemTags.LOGS)
@@ -155,21 +171,6 @@ public class JSGDecorItemTagGenerator extends ItemTagsProvider {
         tag(ItemTags.FENCES)
                 .addTag(JSGDecorItemTags.FENCES);
 
-        tag(ItemTags.LEAVES)
-                .addTag(JSGDecorItemTags.LEAVES);
-
-        tag(ItemTags.LOGS)
-                .addTag(JSGDecorItemTags.LOGS);
-
-        tag(ItemTags.LOGS_THAT_BURN)
-                .addTag(JSGDecorItemTags.LOGS_THAT_BURN);
-
-        tag(ItemTags.PLANKS)
-                .addTag(JSGDecorItemTags.PLANKS);
-
-        tag(ItemTags.SAPLINGS)
-                .addTag(JSGDecorItemTags.SAPLINGS);
-
         tag(ItemTags.SLABS)
                 .addTag(JSGDecorItemTags.SLABS);
 
@@ -178,31 +179,6 @@ public class JSGDecorItemTagGenerator extends ItemTagsProvider {
 
         tag(ItemTags.TRAPDOORS)
                 .addTag(JSGDecorItemTags.TRAPDOORS);
-
-        tag(ItemTags.WOODEN_BUTTONS)
-                .addTag(JSGDecorItemTags.WOODEN_BUTTONS);
-
-        tag(ItemTags.WOODEN_DOORS)
-                .addTag(JSGDecorItemTags.WOODEN_DOORS);
-
-        tag(ItemTags.WOODEN_FENCES)
-                .addTag(JSGDecorItemTags.WOODEN_FENCES);
-
-        tag(ItemTags.WOODEN_PRESSURE_PLATES)
-                .addTag(JSGDecorItemTags.WOODEN_PRESSURE_PLATES);
-
-        tag(ItemTags.WOODEN_SLABS)
-                .addTag(JSGDecorItemTags.WOODEN_SLABS);
-
-        tag(ItemTags.WOODEN_STAIRS)
-                .addTag(JSGDecorItemTags.WOODEN_STAIRS);
-
-        tag(ItemTags.WOODEN_TRAPDOORS)
-                .addTag(JSGDecorItemTags.WOODEN_TRAPDOORS);
-
-        //Minecraft braziers tags
-        tag(ItemTags.PIGLIN_LOVED)
-                .add(BrazierType.HATAK.block().get().asItem());
 
         //Forge Tags
         tag(Tags.Items.FENCES)
@@ -230,6 +206,6 @@ public class JSGDecorItemTagGenerator extends ItemTagsProvider {
                 .addTag(JSGDecorItemTags.BLUE_GLASS);
 
         tag(Tags.Items.GLASS_GREEN)
-                .addTag(JSGDecorItemTags.GREEN_GLASS);*/
+                .addTag(JSGDecorItemTags.GREEN_GLASS);
     }
 }
